619ec74f3e917bee77e3b83904b93ea6b72f28c3
Maajid Siddiqui <msiddiqui.digitals@gmail.com>
2020-12-04 23:58:01 +0500

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const card = ({ name, email, id }) => {
+  return (
+    <div className="tc bg-light-gray dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img src={`https://robohash.org/${id}?200x200`} alt="robots" />
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+};
+
+export default card;
---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Robo 1",
+    username: "robo1",
+    email: "robo1@gmail.com",
+  },
+  {
+    id: 2,
+    name: "Robo 2",
+    username: "robo2",
+    email: "robo2@gmail.com",
+  },
+  {
+    id: 3,
+    name: "Robo 3",
+    username: "robo3",
+    email: "robo3@gmail.com",
+  },
+  {
+    id: 4,
+    name: "Robo 4",
+    username: "robo4",
+    email: "robo4@gmail.com",
+  },
+  {
+    id: 5,
+    name: "Robo 5",
+    username: "robo5",
+    email: "robo5@gmail.com",
+  },
+  {
+    id: 6,
+    name: "Robo 6",
+    username: "robo6",
+    email: "robo6@gmail.com",
+  },
+  {
+    id: 7,
+    name: "Robo 7",
+    username: "robo7",
+    email: "robo7@gmail.com",
+  },
+  {
+    id: 8,
+    name: "Robo 8",
+    username: "robo8",
+    email: "robo8@gmail.com",
+  },
+  {
+    id: 9,
+    name: "Robo 9",
+    username: "robo9",
+    email: "robo9@gmail.com",
+  },
+  {
+    id: 10,
+    name: "Robo 10",
+    username: "robo10",
+    email: "robo10@gmail.com",
+  },
+];
